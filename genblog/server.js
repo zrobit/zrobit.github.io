6bd43167d5af57070ba916387543e4a34f77e1b1
zrobit <luismigre47@gmail.com>
2016-01-12 19:21:35 -0500

    actualizo server para hacer preview del blog

--- genblog/server.js
+++ genblog/server.js
@@ -2,18 +2,20 @@ var express = require('express'),
   jade = require('jade'),
   stylus = require('stylus'),
   nib = require('nib'),
-  handler = require('./handler'),
+  fs = require('fs'),
+  marked = require('marked'),
+  // handler = require('./handler'),
   app = express(),
   host = ['0.0.0.0', 8001];
 
-handler.set_env('dev');
+// handler.set_env('dev');
 
 app.set('view engine', 'jade');
 
 app.use('/static/styles', stylus.middleware({
   debug: true,
   src: __dirname + '/styles',
-  dest: __dirname + '/build/static/styles',
+  dest: __dirname + '../static/styles',
   serve: true,
   forse: false,
   compile: function ( str, path ) {
@@ -21,17 +23,18 @@ app.use('/static/styles', stylus.middleware({
   }
 }));
 
-app.use( '/static/styles', express.static( __dirname + '/build/static/styles' ));
+app.use( '/static/styles', express.static( __dirname + '../static/styles' ));
 app.use( '/static/scripts', express.static( __dirname + '/scripts' ));
 app.use( '/static/images', express.static( __dirname + '/images' ));
 app.use( '/static/fonts', express.static( __dirname + '/fonts' ));
 
 app.get( '/', function(req, res) {
-  res.render( __dirname + '/templates/home.jade', handler);
+  res.render( __dirname + '/layouts/index.jade');
 });
 
-app.get(/^\/([a-z0-9\/\-]+)$/, function(req, res) {
-  res.render( __dirname + '/templates/' + req.params[0] + '.jade', handler );
+app.get(/^\/blog\/([a-z0-9\/\-]+)$/, function(req, res) {
+  file = fs.readFileSync('../_posts/'+req.params[0]+'.md', "utf8");
+  res.render( __dirname + '/layouts/post.jade', {post: marked(file)} );
 });
 
 if ( process.argv[2] ) {
