a01a1fea4ccea27b574b2882159549ec0ef91bac
zrobit <luismigre47@gmail.com>
2016-01-13 05:13:30 -0500

    add gulp task for styles

--- genblog/gulpfile.js/tasks/index.js
+++ genblog/gulpfile.js/tasks/index.js
@@ -1,2 +1,3 @@
 exports.copy = require("./watch.js");
 exports.copy = require("./html.js");
+exports.copy = require("./styles.js");
---
+++ genblog/gulpfile.js/tasks/styles.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    nib = require('nib'),
+    stylus = require('gulp-stylus');
+
+var src = [
+  'styles/**/*.styl',
+  '!styles/modules/**/*',
+  '!styles/sections/**/*',
+  '!styles/templates/**/*'
+  ];
+
+var dest = '../static/styles';
+
+
+function styles () {
+  return gulp.src(src)
+    .pipe(stylus({use: [nib()]}))
+    .pipe(gulp.dest(dest));
+}
+
+gulp.task('styles', styles);
+module.exports = styles;
