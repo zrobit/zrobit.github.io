a0dd8be2ae60e31d2c809998e01f38ea5289631a
zrobit <luismigre47@gmail.com>
2016-01-12 22:04:41 -0500

    add task for build site

---
+++ genblog/gulpfile.js/tasks/html.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    jade = require('jade'),
+    gulp_jade = require('gulp-jade'),
+    marked = require('marked'),
+    fs = require('fs'),
+    through = require('through2');
+
+
+var src = ['../_posts/**/*.md'];
+
+var post_template = fs.readFileSync('layouts/post.jade', "utf8");
+
+
+function htmlPosts () {
+  function render () {
+    function compile (file, enc, cb) {
+      var compiled;
+      var handler = {};
+      var jade_options = {
+        // Jade opciones van aqui :)
+        pretty: true,
+        filename: file.path
+      };
+
+      var marked_options = {
+      };
+
+      compiled = marked(String(file.contents), marked_options)
+      handler.post = compiled;
+      compiled = jade.compile(post_template, jade_options)({post: compiled});
+
+      file.path = file.path.substr(0, file.path.lastIndexOf(".")) + ".html";
+      file.contents = new Buffer(compiled);
+      cb(null, file);
+    }
+    return through.obj(compile);
+  }
+  return gulp.src(src)
+    .pipe(render())
+    .pipe(gulp.dest('../blog/'));
+}
+
+function htmlIndex(){
+  var handler = {};
+  return gulp.src(['layouts/index.jade'])
+    .pipe(gulp_jade({locals: handler, pretty:true}))
+    .pipe(gulp.dest('../'))
+}
+gulp.task('html:posts', htmlPosts);
+gulp.task('html:index', htmlIndex);
